71f56b50dc909a38e10afbe678c92e84d12c5fb1
JAYASINGHE Kushan Menake <jayku@champ.aero>
2017-10-30 18:46:25 +0800

    Initial commit

---
+++ src/main/java/com/champ/admin/console/ChampAdminConsoleApplication.java
@@ -0,0 +1,12 @@
+package com.champ.admin.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampAdminConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampAdminConsoleApplication.class, args);
+	}
+}
---
+++ src/main/java/com/champ/admin/console/web/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.champ.admin.console.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+	
+//	@RequestMapping(value={"/login2"}, method = RequestMethod.GET)
+//	public String login(){
+//		
+//		return "/login2";
+//	}
+
+}
